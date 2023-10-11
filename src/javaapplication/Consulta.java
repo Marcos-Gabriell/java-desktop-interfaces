d737c2d3ea4fab27fd64c14a3acddde4140760f4
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-10-11 19:34:25 -0300

    criação da class de consulta

---
+++ src/javaapplication/Consulta.java
@@ -0,0 +1,16 @@
+
+package javaapplication;
+
+
+public class Consulta {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String data;
+    private boolean jaEraPaciente;
+    private boolean consultaRealizada;
+    private String observacoes;
+
+    // Construtores, getters e setters
+}
+
